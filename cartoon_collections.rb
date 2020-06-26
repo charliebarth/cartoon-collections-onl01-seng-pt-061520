d607bf1be74384a4e495708ffacb10a4d86d773c
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 02:46:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ end
 def summon_captain_planet(array)
   array.collect do |element|
     string = element.join
-    string.
+    string.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
