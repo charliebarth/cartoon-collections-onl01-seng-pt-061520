b6945689e059675e5f6e33265bea7432c24ec7b4
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 03:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.select do |calls|
+  array.select! do |calls|
     calls.size <= 4
   end
+  shorter_calls = array.all? do |call|
+    call.size <= 4
+  end
+  return array
 end
 
 def find_the_cheese# code an argument here
