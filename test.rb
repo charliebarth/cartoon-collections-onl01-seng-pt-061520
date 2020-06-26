081a35278877afa49911121de0cd05a61efc7d13
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 03:04:53 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,6 +2,6 @@ def long_planeteer_calls(array)
   array.select! do |calls|
     calls.size <= 4
   end
-  shorter_calls = array.all?
+  shorter_calls = array.all? do |
 end
 long_planeteer_calls(["earth", "wind", "fire", "interpolate"])
