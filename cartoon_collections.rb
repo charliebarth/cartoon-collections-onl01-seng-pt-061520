020f3ce611514162fe19bd55c56fda5f605a46b8
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 03:08:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.select! do |calls|
-    calls.size <= 4
-  end
+  
   shorter_calls = array.all? do |call|
     call.size <= 4
   end
