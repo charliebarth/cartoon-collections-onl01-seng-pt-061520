e20c6b0df1e30761a69d603be03f00b817b82a59
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 02:59:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.select do |calls|
+    
 end
 
 def find_the_cheese# code an argument here
